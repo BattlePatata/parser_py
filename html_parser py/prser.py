ef2a91799ee42790779b862d71bfff6c618efe99
BattlePatata <108839534+BattlePatata@users.noreply.github.com>
2024-08-31 12:51:28 +0500

    Didn't work with some websites due to the fact that I didn't use headers
    After adding headers all websites started parsing but formating and
    garbage info getting through

--- html_parser py/prser.py
+++ html_parser py/prser.py
@@ -2,18 +2,31 @@ from LxmlSoup import LxmlSoup
 import requests, os
 
 user_site = input('Please enter your website: ')
+headers = {'User-Agent': 'Mozilla/55.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.46'}
 
 try:
-    html = requests.get(f'https://{user_site}').text
+    html = requests.get(f'https://{user_site}', headers=headers).text
 except:
-    html = requests.get(f'http://{user_site}').text
+    html = requests.get(f'http://{user_site}', headers=headers).text
 
 soup = LxmlSoup(html)
 
 # Finding all usefull text
 sec_texts = soup.find_all('section')
-container_texts = soup.find_all('container')
-_texts = sec_texts + container_texts
+_texts = sec_texts
+
+if len(_texts) == 0:
+    container_texts = soup.find_all('container')
+    _texts = container_texts
+
+if len(_texts) == 0:
+    main_texts = soup.find_all('main')
+    _texts = main_texts
+
+if len(_texts) == 0:
+    div_texts = soup.find_all('div')
+    _texts = div_texts
+
 # End of finding all usefull text
 
 # Getting rid off all whitespaces and creating a log file for the results
@@ -24,11 +37,12 @@ if res_file == 0:
 
 for i, data in enumerate(_texts):
     res = data.text()
-    name = ' '.join(res.split())
+    if len(res) > 0:
+        name = ' '.join(res.split())
+        
+        with open(f'{user_site}.txt', 'a', encoding='utf-8') as write_file:
+            write_file.write(f'{name}\n\n')
 
-    with open(f'{user_site}.txt', 'a', encoding='utf-8') as write_file:
-        write_file.write(f'{name}\n\n')
-
-    print()
-    print(name)
+        print()
+        print(name)
 # The end of getting rid off all whitespaces and creating a log file for the results
